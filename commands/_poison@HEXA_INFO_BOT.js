c03eb6892ac3481ae2d4e8bf8318c98bf3042505
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-07-06 00:35:10 +0530

    Update and rename _poison@HEXA_INFO_SOBOT.js to _poison@HEXA_INFO_BOT.js

--- commands/_poison@HEXA_INFO_BOT.js
+++ commands/_poison@HEXA_INFO_BOT.js
@@ -1,5 +1,5 @@
 /*CMD
-  command: /poison@HEXA_INFO_SOBOT
+  command: /poison@HEXA_INFO_BOT
   help: 
   need_reply: false
   auto_retry_time: 
@@ -17,7 +17,7 @@ CMD*/
 
 
 var button = [
-[{title : "SUPPORT" , url : "https://t.me/HEXA_AKATSUKI"}]
+[{title : "SUPPORT" , url : "https://t.me/Pokemon_Group_Telegram"}]
 ]
 
 Bot.sendInlineKeyboard(button, "*Type  :  poison\nStrong Against: Grass, Fairy\nWeak Against: Poison, Ground, Rock, Ghost, Steel\nResistant To: Fighting, Poison, Grass, Fairy, Bug\nVulnerable To: Ground, Psychic*")
