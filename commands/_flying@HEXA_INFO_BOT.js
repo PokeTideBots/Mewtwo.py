9018dd884468ed3a1c6dbae44b67ea50c89c3aca
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-07-05 23:59:01 +0530

    Update and rename _flying@HEXA_INFO_SOBOT.js to _flying@HEXA_INFO_BOT.js

--- commands/_flying@HEXA_INFO_BOT.js
+++ commands/_flying@HEXA_INFO_BOT.js
@@ -1,5 +1,5 @@
 /*CMD
-  command: /flying@HEXA_INFO_SOBOT
+  command: /flying@HEXA_INFO_BOT
   help: 
   need_reply: false
   auto_retry_time: 
@@ -17,7 +17,7 @@ CMD*/
 
 
 var button = [
-[{title : "SUPPORT" , url : "https://t.me/HEXA_AKATSUKI"}]
+[{title : "SUPPORT" , url : "https://t.me/Pokemon_Group_Telegram"}]
 ]
 
 Bot.sendInlineKeyboard(button, "*Type  :  flying\nStrong Against: Fighting, Bug, Grass\nWeak Against: Rock, Steel, Electric\nResistant To: Fighting, Ground, Bug, Grass\nVulnerable To: Rock, Electric, Ice*")
