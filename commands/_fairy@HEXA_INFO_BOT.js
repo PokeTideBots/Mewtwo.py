dba7cdf58bf61b1daacc82cac2179370f9efe716
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-07-05 11:20:09 +0530

    Update and rename _fairy@HEXA_INFO_SOBOT.js to _fairy@HEXA_INFO_BOT.js

--- commands/_fairy@HEXA_INFO_BOT.js
+++ commands/_fairy@HEXA_INFO_BOT.js
@@ -1,5 +1,5 @@
 /*CMD
-  command: /fairy@HEXA_INFO_SOBOT
+  command: /fairy@HEXA_INFO_BOT
   help: 
   need_reply: false
   auto_retry_time: 
@@ -16,7 +16,7 @@
 CMD*/
 
 var button = [
-[{title : "SUPPORT" , url : "https://t.me/HEXA_AKATSUKI"}]
+[{title : "SUPPORT" , url : "https://t.me/Pokemon_Group_Telegram"}]
 ]
 
 Bot.sendInlineKeyboard(button, "*Type  :  fairy\nStrong Against: Fighting, Dragon, Dark\nWeak Against : Poison, Steel, Fire\nResistant To: Fighting, Bug, Dragon, Dark\nVulnerable To: Poison, Steel*")
