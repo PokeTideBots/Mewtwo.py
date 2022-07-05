29b227980fa65405756baa5e6de8747f5db8aead
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-07-05 11:16:26 +0530

    Update and rename _dark@HEXA_INFO_SOBOT.js to _dark@HEXA_INFO_BOT.js

--- commands/_dark@HEXA_INFO_BOT.js
+++ commands/_dark@HEXA_INFO_BOT.js
@@ -1,5 +1,5 @@
 /*CMD
-  command: /dark@HEXA_INFO_SOBOT
+  command: /dark@HEXA_INFO_BOT
   help: 
   need_reply: false
   auto_retry_time: 
@@ -16,7 +16,7 @@
 CMD*/
 
 var button = [
-[{title : "SUPPORT" , url : "https://t.me/HEXA_AKATSUKI"}]
+[{title : "SUPPORT" , url : "https://t.me/Pokemon_Group_Telegram"}]
 ]
 
 Bot.sendInlineKeyboard(button, "*Type  :  dark\nStrong Against: Ghost, Psychic\nWeak Against: Fighting, Dark, Fairy\nResistant To: Ghost, Psychic, Dark\nVulnerable To: Fighting, Bug, Fairy*")
