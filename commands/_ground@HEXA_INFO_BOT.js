6899d71bff99fffb84610945bb288f1bdfecde13
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-07-06 00:02:59 +0530

    Update and rename _ground@HEXA_INFO_SOBOT.js to _ground@HEXA_INFO_BOT.js

--- commands/_ground@HEXA_INFO_BOT.js
+++ commands/_ground@HEXA_INFO_BOT.js
@@ -1,5 +1,5 @@
 /*CMD
-  command: /ground@HEXA_INFO_SOBOT
+  command: /ground@HEXA_INFO_BOT
   help: 
   need_reply: false
   auto_retry_time: 
@@ -16,7 +16,7 @@
 CMD*/
 
 var button = [
-[{title : "SUPPORT" , url : "https://t.me/HEXA_AKATSUKI"}]
+[{title : "SUPPORT" , url : "https://t.me/Pokemon_Group_Telegram"}]
 ]
 
 Bot.sendInlineKeyboard(button, "*Type  :  ground\nStrong Against : Poison,Rock,Steel,Fire,Electric\nWeak Against : Flying, Bug, Grass\nResistant To : Poison,Rock,Electric\nVulnerable To : Water, Grass, Ice*")
