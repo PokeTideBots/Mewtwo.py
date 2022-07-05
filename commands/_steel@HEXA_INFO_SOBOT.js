9bfa2522d3cb2eb5f1330e8c3f3edb1efc69f553
PokeTideBots <103623854+PokeTideBots@users.noreply.github.com>
2022-07-06 00:48:44 +0530

    Update _steel@HEXA_INFO_SOBOT.js

--- commands/_steel@HEXA_INFO_SOBOT.js
+++ commands/_steel@HEXA_INFO_SOBOT.js
@@ -1,5 +1,5 @@
 /*CMD
-  command: /steel@HEXA_INFO_SOBOT
+  command: /steel@HEXA_INFO_BOT
   help: 
   need_reply: false
   auto_retry_time: 
@@ -16,7 +16,7 @@
 CMD*/
 
 var button = [
-[{title : "SUPPORT" , url : "https://t.me/HEXA_AKATSUKI"}]
+[{title : "SUPPORT" , url : "https://t.me/Pokemon_Group_Telegram"}]
 ]
 
 Bot.sendInlineKeyboard(button, "*Type  :  steel\nStrong Against: Rock, Ice, Fairy\nWeak Against: Steel, Fire, Water, Electric\nResistant To: Normal, Flying, Poison, Rock, Bug, Steel, Grass, Psychic, Ice, Dragon, Fairy\nVulnerable To : Fighting, Ground, Fire*")
